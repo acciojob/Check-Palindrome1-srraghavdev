4727de562798d6441fb0a80503a500533a95af3e
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-16 18:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+let start=0
+let end= str.length-1
+	while(end>start){
+		if(s[start]!== s[end]){
+			return false
+		}
+		end--
+		start++
+	}
+	return true
 }
 module.exports = palindrome
